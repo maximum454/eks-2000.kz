9aa3ba683428a2ad52080f620fe26c944d9e4425
maxim <maximum454@gmail.com>
2018-09-04 16:37:36 +0600

    nav header

--- src/js/eks2000.js
+++ src/js/eks2000.js
@@ -1,6 +1,39 @@
 //= partials/jquery.min.js
-//= partials/bootstrap.js
+//= partials/slick.js
 $(function () {
-
+    $('.js-partners').slick({
+        dots: false,
+        infinite: false,
+        speed: 300,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+        ]
+    });
 });
 
